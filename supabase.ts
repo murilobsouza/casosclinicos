abe09de5d55285962cb1f5bd5ac9a1064786ee80
murilobsouza <murilobsouza@gmail.com>
2026-01-31 15:29:10 -0300

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,5 +1,5 @@
 
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@^2.45.0';
+import { createClient } from '@supabase/supabase-js';
 
 const getEnv = (key: string): string => {
   try {
