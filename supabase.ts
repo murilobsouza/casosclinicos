e89d872ea6abf4b800663a0680c9439aadd4ced9
murilobsouza <murilobsouza@gmail.com>
2026-02-03 11:29:22 -0300

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Access environment variables using process.env to resolve TypeScript errors related to ImportMeta.
-const supabaseUrl = process.env.VITE_SUPABASE_URL || 'https://plvoogvaddxdwqyeqydt.supabase.co';
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_OG1foQAiLcKMjRFmdF7iew_423mA8oV';
+const supabaseUrl = process.env.VITE_SUPABASE_URL || 'https://qmbpafyvpjziiudahmlx.supabase.co';
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_dcNQd8zJwIzV5WzTFR41TQ_pX4kyeA8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
